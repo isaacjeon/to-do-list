1fa0c4ae9275bc43a42a13559ce97a5530a24374
Isaac Jeon <43486715+isaacjeon@users.noreply.github.com>
2018-09-30 03:32:26 -0700

    Update list-create.js

--- list-create.js
+++ list-create.js
@@ -1,1 +1,15 @@
+var newItem = document.getElementById("newItem");
+var add = document.getElementById("add");
+var itemList = document.getElementById("itemList");
 
+newItem.onclick = function() {addItem()};
+
+function addItem(){
+  var button = document.createElement("button");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode("X"));
+  li.appendChild(document.createTextNode(newItem.value));
+  itemList.appendChild(button);
+  itemList.appendChild(li);
+  newItem.value = "";
+}
